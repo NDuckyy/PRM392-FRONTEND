2f67b10ee6cb48680a1c1c8936fb8ccd9656c04a
QuanDong <quanonepunch@gmail.com>
2025-10-28 11:34:15 +0700

    fix merge

--- app/src/main/java/com/example/prm392_frontend/ProductListActivity.java
+++ app/src/main/java/com/example/prm392_frontend/ProductListActivity.java
@@ -1,6 +1,9 @@
 package com.example.prm392_frontend;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.prm392_frontend.api.ApiClient;
